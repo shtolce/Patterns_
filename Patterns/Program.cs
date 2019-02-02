8d85324e7633b54b4685e55b44a47af02c92c50c
shtolce <kimdimka@yandex.ru>
2019-02-02 15:22:53 +1000

    Перестроил структуру проекта

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -4,17 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Patterns.Classes.FactoryMethod;
+using Patterns.Classes.AbstractFactory;
 
 namespace Patterns
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Фабричный метод
+        /// </summary>
+        private static void FactoryMethodRun()
         {
             Product prA = new CreatorA().Create();
             Product prB = new CreatorB().Create();
             prA.Method1();
             prB.Method1();
+        }
+        /// <summary>
+        /// Абстрактная фабрика
+        /// </summary>
+        private static void AbstractFactoryRun()
+        {
+            Product prA = new CreatorA().Create();
+            Product prB = new CreatorB().Create();
+            prA.Method1();
+            prB.Method1();
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            AbstractFactoryRun();
+
+
+
+
 
 
         }
